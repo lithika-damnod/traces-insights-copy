3d7fa2efe40d74a3cda395b71b70cc6a4a6cbfda
erandi-perera <erandi.20231149@iit.ac.lk>
2025-03-18 08:02:46 +0530

    Added the uturn arrow button at the top of the map view to easy navigation

--- lib/pages/shipment/views/edit/view_location_view.dart
+++ lib/pages/shipment/views/edit/view_location_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:traces/pages/shipment/views/edit/view_shipping_info_view.dart';
 import 'package:traces/shared/widgets/modal_bottom_sheet.dart';
@@ -10,50 +11,70 @@ class ViewLocationView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ModalBottomSheet(
       showCloseButton: false,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
+      child: Stack(
         children: [
-          /// **Map Preview** inside a container
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10.0),
-                topRight: Radius.circular(10.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              /// **Map Preview** inside a container
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0),
+                  ),
+                  color: Colors.black12,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0),
+                  ),
+                  child: Image.asset(
+                    'assets/icons/map_expanded.png', // Replace with the actual map view
+                    width: double.infinity,
+                    height: 320,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              color: Colors.black12,
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10.0),
-                topRight: Radius.circular(10.0),
+
+              /// **Shipping Address**
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "No. 25, Dambulla Road, Kurunegala, North Western, 60000",
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(255, 255, 255, 0.7),
+                          fontFamily: "SF Pro Text",
+                          letterSpacing: -0.425),
+                    ),
+                  ],
+                ),
               ),
-              child: Image.asset(
-                'assets/icons/map_expanded.png', // Replace with the actual asset path
-                width: double.infinity,
-                height: 320,
-                fit: BoxFit.cover,
-              ),
-            ),
+            ],
           ),
 
-          /// **Shipping Address**
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  "No. 25, Dambulla Road, Kurunegala, North Western, 60000",
-                  style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromRGBO(255, 255, 255, 0.7),
-                      fontFamily: "SF Pro Text",
-                      letterSpacing: -0.425),
-                ),
-              ],
+          /// **Top-right floating button**
+          Positioned(
+            top: 10.0,
+            right: 10.0,
+            child: CircularIconButton(
+              icon: CupertinoIcons.arrow_uturn_left,
+              iconColor: Color(0xFF0A84FF),
+              backgroundColor: Color(0xFF1C1C1E),
+              onPressed: () {
+                final modal =
+                    context.findAncestorStateOfType<ModalBottomSheetState>();
+                modal?.navigateTo(ViewShippingInformationView());
+              },
             ),
           ),
         ],
