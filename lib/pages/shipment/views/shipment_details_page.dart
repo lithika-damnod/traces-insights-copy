3e48a99570658b6ffba24c197e2dca464b70c133
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-03-18 19:42:48 +0530

    Implemented the Page Navigation Component  on the Shipment Details Pages

--- lib/pages/shipment/views/shipment_details_page.dart
+++ lib/pages/shipment/views/shipment_details_page.dart
@@ -1,100 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:traces/pages/shipment/views/options/options_view.dart';
-import 'package:traces/shared/widgets/modal_bottom_sheet.dart';
-import 'package:traces/shared/widgets/order_confirmation_view.dart';
+import 'package:traces/pages/shipment/views/shipment_details_page.dart';
+
+import '../../../shared/widgets/test.dart';
+import '../../../shared/widgets/shipment_details_page_initial.dart';
+
+
 
 class ShipmentDetailsPage extends StatefulWidget {
-  final String orderId;
-  const ShipmentDetailsPage({ required this.orderId,super.key});
-
-
   @override
-  State<ShipmentDetailsPage> createState() => _ShipmentDetailsPageState();
+   _ShipmentDetailsPageState createState() => _ShipmentDetailsPageState();
+
+    const   ShipmentDetailsPage({super.key});
 }
 
 class _ShipmentDetailsPageState extends State<ShipmentDetailsPage> {
+  final PageController _controller = PageController();
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-
-
-    // TODO: Start replacing content from here
     return Scaffold(
-      appBar: CupertinoNavigationBar(
-        padding: EdgeInsetsDirectional.all(0.0),
-        backgroundColor: Colors.black,
-        middle: Text(
-          " #${widget.orderId}",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
-          ),
-        ),
-        leading: Navigator.of(context).canPop()
-            ? CupertinoNavigationBarBackButton(
-                color: Color(0xFF0A84FF),
-                previousPageTitle: "Back",
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-            : null,
-        trailing: IconButton(
-          onPressed: () {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) => ModalBottomSheet(
-                /* content starts here */
-                child: OptionsView(),
-                /* content ends here */
-              ),
-            );
-          },
-          icon: Icon(
-            CupertinoIcons.ellipsis_circle,
-            size: 24.0,
-            color: Color(0xFF0A84FF),
-          ),
-        ),
-      ),
-      backgroundColor: Colors.black,
-      body: Container(
-        color: Colors.black,
-        child: Column(
-
-          children: [Expanded(
-            child: ListView(
-
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _controller,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
               children: [
-
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Icon(Icons.flag,
-                        color: Colors.yellow,size: 20,),
-                        decoration: BoxDecoration(
-                          color:Colors.yellow.withOpacity(0.2),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                      ),
-                      SizedBox(
-                          width: 10,
-                          height: 0,
-                      ),
-
-                      Text("IN TRANSIT"),
-                    ],
-                  ),
-                )
+                ShipmentDetailsPageInitial(orderId: "58"),
+                Container(color: Colors.red),
               ],
             ),
-          )
-          ],
-
-        ),
-
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(2, (index) {
+              return Container(
+                margin: EdgeInsets.all(5),
+                width: 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == index ? Colors.white : Colors.grey,
+                ),
+              );
+            }),
+          ),
+          SizedBox(height: 20),
+        ],
       ),
     );
   }
