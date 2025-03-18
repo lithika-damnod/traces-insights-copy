051d0d79f131b9dc0c2055ec6d5a3f4ec1d9473d
Thulani Aponso <thulaniaponso@gmail.com>
2025-03-18 15:56:45 +0530

    Adjusted icon size, padding, and spacing for a more balanced UI

--- lib/pages/home/views/add_shipment/add_shipment_scan_view.dart
+++ lib/pages/home/views/add_shipment/add_shipment_scan_view.dart
@@ -1,38 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:traces/pages/home/views/add_shipment/add_shipment_manual_view.dart';
-// import 'package:traces/shared/widgets/modal_bottom_sheet.dart';
-
-// class AddShipmentScanView extends StatelessWidget {
-//   const AddShipmentScanView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: Start replacing content from here
-//     return Column(
-//       mainAxisSize: MainAxisSize.min,
-//       children: [
-//         Padding(
-//           padding: const EdgeInsets.only(top: 26.0),
-//           child: Column(
-//             children: [
-//               Text("Link New Shipment (QR Scan)",
-//                   style: TextStyle(fontSize: 18.0)),
-//               SizedBox(height: 10.0),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   final modal =
-//                       context.findAncestorStateOfType<ModalBottomSheetState>();
-//                   modal?.navigateTo(AddShipmentManualView());
-//                 },
-//                 child: Text("Enter Manually"),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:traces/pages/home/views/add_shipment/add_shipment_manual_view.dart';
@@ -44,7 +9,7 @@ class AddShipmentScanView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
       ),
@@ -58,47 +23,55 @@ class AddShipmentScanView extends StatelessWidget {
                 "Link New Shipment",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 18.0,
+                  fontSize: 21.0,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
               ),
               IconButton(
-                icon: const Icon(
-                  CupertinoIcons.arrow_turn_up_left,
-                  color: Colors.blue,
+                icon: Container(
+                  width: 43.56,
+                  height: 43.56,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[900],
+                  ),
+                  child: const Icon(
+                    CupertinoIcons.arrow_turn_up_left,
+                    color: Colors.blue,
+                  ),
                 ),
                 onPressed: () {
-                  Navigator.pop(context);
+                  final modal =
+                      context.findAncestorStateOfType<ModalBottomSheetState>();
+                  modal?.navigateTo(AddShipmentManualView());
                 },
               ),
             ],
           ),
-          const SizedBox(height: 8.0),
           const Text(
             "Provide your shipment details to link it to your account. You can enter the information manually or quickly scan the QR code for faster input.",
             style: TextStyle(
-              fontSize: 14.0,
-              color: Colors.white70,
+              fontSize: 17,
+              color: Color.fromRGBO(255, 255, 255, 0.6),
             ),
           ),
           const SizedBox(height: 20.0),
           Container(
-            height: 200.0,
+            height: 290.0,
             width: double.infinity,
             decoration: BoxDecoration(
               color: Colors.black,
-              borderRadius: BorderRadius.circular(12.0),
+              borderRadius: BorderRadius.circular(14.0),
             ),
             child: Center(
               child: Icon(
                 CupertinoIcons.qrcode_viewfinder,
-                size: 80.0,
-                color: Colors.white60,
+                size: 180.0,
+                color: Colors.white30,
               ),
             ),
           ),
-          const SizedBox(height: 20.0),
         ],
       ),
     );
